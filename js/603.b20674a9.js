8ceef6b577201630d593d43a5e2eda9755b9dfeb
leandro16197 <leandroovejero16197@gmail.com>
2026-03-27 14:06:10 -0300

    Updates

---
+++ js/603.b20674a9.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkportfolio"]=self["webpackChunkportfolio"]||[]).push([[603],{603:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var o=u(641);const r={class:"about"};function a(n,t){return(0,o.uX)(),(0,o.CE)("div",r,t[0]||(t[0]=[(0,o.Lk)("h1",null,"This is an about page",-1)]))}var e=u(6262);const s={},c=(0,e.A)(s,[["render",a]]);var i=c}}]);
+//# sourceMappingURL=603.b20674a9.js.map
